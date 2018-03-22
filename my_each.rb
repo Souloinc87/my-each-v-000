c578ae98f5e4227a6659539a2875bfcda39455eb
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-22 02:41:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,9 @@ def my_each(array)# put argument(s) here
   # code here
   counter = 0
 
-  while counter < array.length 
+  while counter < array.length
     yield array[counter]
     counter += 1
   end
-  
+ array 
 end
